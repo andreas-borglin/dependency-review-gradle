cc4347503bf3eacb054458c35e58fac5b925ce99
Andreas Borglin <andreas.borglin+github@gmail.com>
2025-08-20 13:35:28 +0200

    Add commons-lang 2.6 with known vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("commons-lang:commons-lang:2.6")
 }
 
 tasks.test {
